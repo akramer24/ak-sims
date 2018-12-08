74c9a85d6dbc0c3531c85c652bd2bb0b7aea2eca
Ari Kramer <arikramer24@gmail.com>
2018-12-08 09:23:36 -0500

    push build

---
+++ build/precache-manifest.2051677142c738f210efa08db4f3575a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54148531d37965cea569",
+    "url": "static/js/runtime~main.54148531.js"
+  },
+  {
+    "revision": "725f39a06ea964d5599e",
+    "url": "static/js/main.725f39a0.chunk.js"
+  },
+  {
+    "revision": "5cbdfde138ecb91189a2",
+    "url": "static/js/1.5cbdfde1.chunk.js"
+  },
+  {
+    "revision": "725f39a06ea964d5599e",
+    "url": "static/css/main.44e5da16.chunk.css"
+  },
+  {
+    "revision": "3fd2e3d31da38742be12d833cd2927ea",
+    "url": "index.html"
+  }
+];
